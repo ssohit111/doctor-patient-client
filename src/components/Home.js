eede9cc746d8b21a9dc9b349c4536e7777d9bf03
ssohit111 <ssohit111@gmail.com>
2022-01-07 17:38:53 +0530

    added video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import '../App.css'
+import Animation from './video/Animation.mp4';
 const Home = () => {
     return (
         <div className='myclass'>
-            <img src="https://images.pexels.com/photos/1692693/pexels-photo-1692693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img_class' />
+            {/* <img src="https://images.pexels.com/photos/1692693/pexels-photo-1692693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img_class' /> */}
+            <video autoPlay loop className='videoclass'>
+                <source src={Animation} type="video/mp4" />
+            </video>
         </div>
     )
 }
